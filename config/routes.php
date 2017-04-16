544d20336120d21b71a5394e089fa2c77b437d90
Aymard KOUAKOU <aymard.pro@gmail.com>
2017-04-16 13:50:26 +0000

    Majors updates...

--- config/routes.php
+++ config/routes.php
@@ -1,13 +1,12 @@
 <?php
-use Cake\Core\Configure;
-use Cake\Filesystem\File;
+
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
 
 Router::plugin(
 	'CakeSwagger',
-	['path' => '/api/doc'],
+	['path' => '/api'],
 	function (RouteBuilder $routes) {
 		$routes->fallbacks(DashedRoute::class);
 	}
